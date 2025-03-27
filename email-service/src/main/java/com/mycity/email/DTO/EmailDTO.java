ccf4031623c6490ee616b85d4a52832e3ceb7fac
mahee37 <mahendraachari37@gmail.com>
2025-03-27 22:43:58 +0530

    Email Service

---
+++ email-service/src/main/java/com/mycity/email/DTO/EmailDTO.java
@@ -0,0 +1,13 @@
+package com.mycity.email.DTO;
+
+public class EmailDTO {
+
+
+	 	private String recipientEmail;
+	    private String subject;
+	    private String message;
+
+	    public EmailDTO() {}
+	
+
+}
