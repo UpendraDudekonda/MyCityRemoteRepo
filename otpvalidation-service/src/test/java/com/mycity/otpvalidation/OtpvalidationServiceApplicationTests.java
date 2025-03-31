e60a79df610a79f253bcfbd5c8af8d98485df7d0
mahee37 <mahendraachari37@gmail.com>
2025-03-31 15:44:19 +0530

    Otp Validation Service

---
+++ otpvalidation-service/src/test/java/com/mycity/otpvalidation/OtpvalidationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycity.otpvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtpvalidationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
