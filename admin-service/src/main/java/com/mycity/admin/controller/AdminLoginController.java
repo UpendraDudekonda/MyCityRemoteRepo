246fe8577e79c0d5a9423f7dad78a6f741ac289b
mahee37 <mahendraachari37@gmail.com>
2025-04-01 11:29:07 +0530

    Merge branch 'main' of https://github.com/MyCityOrganization/MyCityRemoteRepo.git

--- admin-service/src/main/java/com/mycity/admin/controller/AdminLoginController.java
+++ admin-service/src/main/java/com/mycity/admin/controller/AdminLoginController.java
@@ -1,6 +1,20 @@
 package com.mycity.admin.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.mycity.admin.entity.Admin;
+
+@Controller
 public class AdminLoginController {
 	// - login();
+	
+	
+	
+	public ResponseEntity<Admin> Login(@RequestBody Admin admin){
+		return null;
+		
+	}
 
 }
