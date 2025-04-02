8eb26ba6dd52f7e45ab5fa23374259e945f83bf9
mahee37 <mahendraachari37@gmail.com>
2025-04-02 18:38:20 +0530

    Merge branch 'main' of https://github.com/Mahee37/MyCityRemoteRepo.git

--- api-gateway/src/main/java/com/mycity/apigateway/controller/LoginController.java
+++ api-gateway/src/main/java/com/mycity/apigateway/controller/LoginController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import com.mycity.apigateway.dto.UserLoginRequest;
+import com.mycity.apigateway.dto.UserRegRequest;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -17,22 +18,20 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/auth/login")
 @RequiredArgsConstructor
 public class LoginController {
-	
-private final WebClient.Builder webClientBuilder;
-	
-	 private static final String USER_SERVICE_NAME = "USER-SERVICE"; // Using service name for discovery
-	    private static final String USER_LOGIN_PATH = "/auth/user/login";
 
-	    @PostMapping("/user")
-	    public Mono<ResponseEntity<String>> loginUser(@Valid @RequestBody UserLoginRequest request) { // Renamed method to loginUser
-	        return webClientBuilder.build()
-	                .post()
-	                .uri("lb://" + USER_SERVICE_NAME + USER_LOGIN_PATH) // Using service name and correct path
-	                .body(Mono.just(request), UserLoginRequest.class)
-	                .retrieve()
-	                .toEntity(String.class)
-	                .map(response -> ResponseEntity.status(response.getStatusCode()).body(response.getBody()));
-	    }
-	 
-	 
+    private final WebClient.Builder webClientBuilder;
+
+    private static final String USER_SERVICE_NAME = "USER-SERVICE"; // Using service name for discovery
+    private static final String USER_LOGIN_PATH = "/auth/user/login";
+
+    @PostMapping("/user")
+    public Mono<ResponseEntity<String>> LoginUser(@RequestBody UserLoginRequest request) {
+        return webClientBuilder.build()
+                .post()
+                .uri("lb://" +USER_SERVICE_NAME + USER_LOGIN_PATH)
+                .body(Mono.just(request), UserLoginRequest.class)
+                .retrieve()
+                .toEntity(String.class)
+                .map(response -> ResponseEntity.status(response.getStatusCode()).body(response.getBody()));
+    }
 }
