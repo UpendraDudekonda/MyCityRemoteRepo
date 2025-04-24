293e47d3f2ff1b8ab97d12c48da448d5618bd82b
UpendraDudekonda <141215379+UpendraDudekonda@users.noreply.github.com>
2025-04-24 15:21:44 +0530

    Revert "Place Methods Updated,Category Service Updated,Media Serivce Updated "

--- place-service/src/main/java/com/mycity/place/controller/PlaceController.java
+++ place-service/src/main/java/com/mycity/place/controller/PlaceController.java
@@ -16,9 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.mycity.place.entity.Place;
 import com.mycity.place.service.PlaceServiceInterface;
-import com.mycity.shared.placedto.PlaceCategoryDTO;
 import com.mycity.shared.placedto.PlaceDTO;
-import com.mycity.shared.placedto.PlaceResponseDTO;
 
 @RestController
 @RequestMapping("/place")
@@ -34,12 +32,13 @@ public class PlaceController {
         return new ResponseEntity<>(msg, HttpStatus.CREATED);
     }
 
+    // Get place by ID (Place entity)
     @GetMapping("/get/{placeId}")
-    public ResponseEntity<PlaceResponseDTO> getPlaceDetails(@PathVariable Long placeId) {
-        PlaceResponseDTO place = placeService.getPlace(placeId);
-        return new ResponseEntity<>(place, HttpStatus.OK);
+    public ResponseEntity<Place> getPlaceDetails(@PathVariable Long placeId) {
+        Place place = placeService.getPlace(placeId);
+        return place != null ? new ResponseEntity<>(place, HttpStatus.OK)
+                             : new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
-
 
     // Update place using PlaceDTO
     @PutMapping("/update/{placeId}")
@@ -64,17 +63,24 @@ public class PlaceController {
 
     
     @GetMapping("/allplaces")
-    public ResponseEntity<List<PlaceResponseDTO>> getAllPlaces() {
+    public ResponseEntity<Object> getAllPlaces() {
         return ResponseEntity.ok(placeService.getAllPlaces());
     }
 
-
+    
+    @GetMapping("/placeby/{id}")
+    public ResponseEntity<Place> getPlaceById(@PathVariable Long id) {
+        Place place = placeService.getPlaceById(id);
+        return place != null ? ResponseEntity.ok(place) : ResponseEntity.notFound().build();
+    }
+    
     @GetMapping("/places/categories")
-    public ResponseEntity<List<PlaceCategoryDTO>> getAllDistinctCategories() {
-        List<PlaceCategoryDTO> categories = placeService.getAllDistinctCategories();
+    public ResponseEntity<List<String>> getAllDistinctCategories() {
+        List<String> categories = placeService.getAllDistinctCategories();
         return ResponseEntity.ok(categories);
     }
-
+    
+    
     
     
 }
--- place-service/src/main/java/com/mycity/place/entity/TimeZone.java
+++ place-service/src/main/java/com/mycity/place/entity/TimeZone.java
@@ -2,6 +2,7 @@ package com.mycity.place.entity;
 
 import java.time.LocalTime;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -21,19 +22,19 @@ import lombok.Setter;
 @NoArgsConstructor
 public class TimeZone
 {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long timeZoneId;
-
-
-    @NonNull
-    private LocalTime openingTime;
-
-    @NonNull
-    private LocalTime closingTime;
-    
-    @OneToOne
-    @JoinColumn(name = "place_id", referencedColumnName = "placeId")
-    private Place place;
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long timeZoneId;
+	
+	@OneToOne(targetEntity = Place.class,cascade = CascadeType.ALL)
+	@JoinColumn(name="place_id",referencedColumnName = "placeId")
+	private Place place;
+	
+	@NonNull
+	private LocalTime openingTime;
+	
+	@NonNull
+	private LocalTime closingTime;
+	
+	
 }
