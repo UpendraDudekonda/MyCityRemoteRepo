4bb4c0596b72d00243f7b3005d33e56874cd62b2
nikhilkatna <nikhilkatna123@gmail.com>
2025-05-13 11:51:30 +0530

    Nikhil - Place-service,email,otp test cases

--- admin-service/src/main/java/com/mycity/admin/config/SecurityConfig.java
+++ admin-service/src/main/java/com/mycity/admin/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.mycity.admin.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -24,7 +25,8 @@ public class SecurityConfig {
 	                .requestMatchers(
 	                    "/admin/auth/internal/login",
 	                	"/admin/profile",
-	                	"/admin/event/**"
+	                	"/admin/event/**",
+	                	"/admin/addPlace"
 	                ).permitAll()
 	                .anyRequest().authenticated()
 	            );
@@ -49,6 +51,7 @@ public class SecurityConfig {
     }
     
     @Bean
+    @LoadBalanced
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder();
     }
